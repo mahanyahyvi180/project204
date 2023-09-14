cf65d1def69b33d3d51135b6e7dd2bf5f395434b
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-09-14 10:49:40 +0330

    Add files via upload

---
+++ ConsoleApp134/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp134
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"power(10)={power(10)}");
+            Console.WriteLine($"power(2,10)={power(2, 10)}");
+        }
+
+            static int power(int baseValue,int exponentValue=2)
+            {
+                int result = 1;
+
+                for (int i = 1; i <= exponentValue; i++)
+                {
+                    result *= baseValue;
+                }
+                return result;
+            }
+        }
+    }
+
